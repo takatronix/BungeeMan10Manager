4492edd4368f4481346727104e478c030e7cf795
takatronix <takatronix@gmail.com>
2020-02-19 04:40:35 +0900

    Initial commit

---
+++ src/main/kotlin/red/man10/bungee/TemplateCommand.kt
@@ -0,0 +1,16 @@
+package red.man10.bungee
+
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.chat.ComponentBuilder
+import net.md_5.bungee.api.plugin.Command
+
+
+public class TemplateCommand: Command {
+
+    constructor(name:String,permission:String): super(name,permission)
+
+    override fun execute(commandSender: CommandSender, strings: Array<String?>?) {
+        commandSender.sendMessage(*ComponentBuilder("Hello world!").color(ChatColor.GREEN).create())
+    }
+}
