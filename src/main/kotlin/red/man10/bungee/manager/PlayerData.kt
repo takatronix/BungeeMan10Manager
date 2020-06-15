f2fb2648b231866011df0e5427a7e23013f303bc
Takashi Ohtsuka <takatronix@gmail.com>
2020-06-15 23:34:52 +0900

    ステータス処理追加

--- src/main/kotlin/red/man10/bungee/manager/PlayerData.kt
+++ src/main/kotlin/red/man10/bungee/manager/PlayerData.kt
@@ -14,7 +14,7 @@ enum class PlayerStatus{
     OK,
     Muted,
     Jailed,
-    Kicked,
+    TempBanned,
     Banned,
 }
 
@@ -22,12 +22,39 @@ class PlayerData {
     lateinit var uuid: UUID
     lateinit var mcid: String
 
+    var freezeUntil: Date? = null      //      拘束期限
+    var muteUntil: Date? = null        //      ミュート期限
+    var jailUntil: Date? = null        //      ジェイル期限
+    var banUntil: Date? = null         //      BAN期限
+
+    fun isFronzen() : Boolean{
+        if(freezeUntil == null)
+            return false
+        return true
+    }
+    fun isMuted() : Boolean{
+        if(muteUntil == null)
+            return false
+        return true
+    }
+    fun isJailed() : Boolean{
+        if(jailUntil == null)
+            return false
+        return true
+    }
+    fun isBanned() : Boolean{
+        if(banUntil == null)
+            return false
+        return true
+    }
+
+
 
     //   チャットとコマンドを履歴に登録する
     //   条件によって Mute/Jail/Kickを返す
 
     fun Add(commandOrChat:String): PlayerStatus{
-        
+
         return PlayerStatus.OK;
     }
 
