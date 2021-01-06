f413ae0723b5ed752ec85a2a76c23df97474aba3
forest611 <forest611328@gmail.com>
2021-01-06 16:34:32 +0900

    ミュート、Jailコマンド追加

--- src/main/kotlin/red/man10/bungee/manager/command/JailCommand.kt
+++ src/main/kotlin/red/man10/bungee/manager/command/JailCommand.kt
@@ -1,17 +1,78 @@
 package red.man10.bungee.manager.command
 
 import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.ComponentBuilder
 import net.md_5.bungee.api.plugin.Command
+import red.man10.bungee.manager.Man10BungeePlugin
 import red.man10.bungee.manager.Man10BungeePlugin.Companion.plugin
+import red.man10.bungee.manager.PlayerData
+import java.text.SimpleDateFormat
 
 object JailCommand : Command("mjail","bungeemanager.jail"){
 
     override fun execute(sender: CommandSender?, args: Array<out String>?) {
 
         if (sender==null)return
-        if (args==null)return
 
-        plugin.log("jail command")
+        //mjail <forest611> <100d> <reason>
+        if (!args.isNullOrEmpty() && args.size == 3){
+
+            val p = args[0]
+
+            val data = PlayerData.getData(p)
+
+            if (data ==null){
+                sender.sendMessage(*ComponentBuilder("§4存在しないユーザーです").create())
+                return
+            }
+
+            val unit = args[1][args[1].length - 1]
+
+            val time: Int
+            try {
+                time = args[1].replace(unit.toString(), "").toInt()
+            } catch (e: Exception) {
+                sender.sendMessage(*ComponentBuilder("§c§l時間の指定方法が不適切です").create())
+                return
+            }
+
+            if (!data.isJailed() && time <0){
+                sender.sendMessage(*ComponentBuilder("§c§lこのユーザーは既に釈放されています！").create())
+                return
+            }
+
+            when(unit){
+
+                'd' ->data.addJailTime(0,0,time)
+                'h' ->data.addJailTime(0,time,0)
+                'm' ->data.addJailTime(time,0,0)
+
+                else -> {
+                    sender.sendMessage(*ComponentBuilder("§c§l時間の指定方法が不適切です").create())
+                    return
+                }
+
+            }
+
+            if (!data.isJailed()){
+                ProxyServer.getInstance().broadcast(*ComponentBuilder("§c§l${p}は釈放されました").create())
+                Man10BungeePlugin.playerDataDic[data.uuid] = data
+                return
+            }
+
+            ProxyServer.getInstance().broadcast(*ComponentBuilder("§c§l${p}は「${args[2]}」の理由により、Jailされました！").create())
+            ProxyServer.getInstance().broadcast(*ComponentBuilder("§c§l釈放日:${SimpleDateFormat("yyyy/MM/dd").format(data.jailUntil)}").create())
+            Man10BungeePlugin.playerDataDic[data.uuid] = data
+
+
+            return
+
+        }
+
+        sender.sendMessage(*ComponentBuilder("§d§l/mjail <mcid> <期間+(d/h/m)> <Jail理由>").create())
+        sender.sendMessage(*ComponentBuilder("§d§l期間をマイナスにすると期間が縮みます").create())
+
     }
 
 }
--- src/main/kotlin/red/man10/bungee/manager/command/MuteCommand.kt
+++ src/main/kotlin/red/man10/bungee/manager/command/MuteCommand.kt
@@ -1,17 +1,80 @@
 package red.man10.bungee.manager.command
 
 import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.ComponentBuilder
 import net.md_5.bungee.api.plugin.Command
+import red.man10.bungee.manager.Man10BungeePlugin
 import red.man10.bungee.manager.Man10BungeePlugin.Companion.plugin
+import red.man10.bungee.manager.PlayerData
+import java.text.SimpleDateFormat
 
 object MuteCommand : Command("mmute","bungeemanager.mute"){
 
     override fun execute(sender: CommandSender?, args: Array<out String>?) {
 
         if (sender==null)return
-        if (args==null)return
 
-        plugin.log("Mute command")
+
+        //mban <forest611> <100d> <reason>
+        if (!args.isNullOrEmpty() && args.size == 3){
+
+            val p = args[0]
+
+            val data = PlayerData.getData(p)
+
+            if (data ==null){
+                sender.sendMessage(*ComponentBuilder("§4存在しないユーザーです").create())
+                return
+            }
+
+            val unit = args[1][args[1].length - 1]
+
+            val time: Int
+            try {
+                time = args[1].replace(unit.toString(), "").toInt()
+            } catch (e: Exception) {
+                sender.sendMessage(*ComponentBuilder("§c§l時間の指定方法が不適切です").create())
+                return
+            }
+
+            if (!data.isMuted() && time <0){
+                sender.sendMessage(*ComponentBuilder("§c§lこのユーザーは既にミュート解除されています！").create())
+                return
+            }
+
+            when(unit){
+
+                'd' ->data.addMuteTime(0,0,time)
+                'h' ->data.addMuteTime(0,time,0)
+                'm' ->data.addMuteTime(time,0,0)
+
+                else -> {
+                    sender.sendMessage(*ComponentBuilder("§c§l時間の指定方法が不適切です").create())
+                    return
+                }
+
+            }
+
+            if (!data.isMuted()){
+                ProxyServer.getInstance().broadcast(*ComponentBuilder("§c§l${p}はミュート解除されました").create())
+                Man10BungeePlugin.playerDataDic[data.uuid] = data
+                return
+            }
+
+
+            ProxyServer.getInstance().getPlayer(p)!!.sendMessage(*ComponentBuilder("§c§lあなたは「${args[2]}」の理由により、ミュートされました！").create())
+            ProxyServer.getInstance().getPlayer(p)!!.sendMessage(*ComponentBuilder("§c§l解除日:${SimpleDateFormat("yyyy/MM/dd").format(data.muteUntil)}").create())
+            Man10BungeePlugin.playerDataDic[data.uuid] = data
+
+
+            return
+
+        }
+
+        sender.sendMessage(*ComponentBuilder("§d§l/mmute <mcid> <期間+(d/h/m)> <ミュート理由>").create())
+        sender.sendMessage(*ComponentBuilder("§d§l期間をマイナスにすると期間が縮みます").create())
+
     }
 
 }
