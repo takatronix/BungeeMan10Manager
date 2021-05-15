c08e2bc1fe003d916f10dc5ee423c4bb2eb43eb3
forest611 <forest611328@gmail.com>
2021-05-15 10:04:44 +0900

    コネクションログを取る処理

---
+++ src/main/kotlin/red/man10/bungee/manager/db/ConnectionDatabase.kt
@@ -0,0 +1,20 @@
+package red.man10.bungee.manager.db
+
+import net.md_5.bungee.api.connection.ProxiedPlayer
+
+object ConnectionDatabase {
+
+    fun joinServer(p:ProxiedPlayer){
+
+
+    }
+
+    fun changeServer(p:ProxiedPlayer,server:String){
+
+    }
+
+    fun leaveServer(p:ProxiedPlayer){
+
+    }
+
+}
